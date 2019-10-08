3de7016054afd05bee850b88dbc49decb04c3ed3
Inigopees <iperezes18dw@ikzubirimanteo.com>
2019-10-08 10:45:55 +0200

    agenda funciona completa, y añadido index.php

--- agenda.php
+++ agenda.php
@@ -1,10 +1,16 @@
 <?php
-	session_start();
+session_start();
+if(isset($_POST["envioIndex"])){
+	$_SESSION["NombUsuario"]=$_POST["nombreIndex"];
+}
 ?>
 <html>
 	<head>
 		<title>Agenda</title>
 		<style type="text/css">
+			h1{
+				text-align: center;
+			}
 			#formulario{
 				background-color: blue;
 				width: 20%;
@@ -18,10 +24,11 @@
 		</style>
 	</head>
 	<body>
+		<h1>Formulario de <?php echo $_SESSION["NombUsuario"];?></h1>
 		<div id="formulario">
 			<form method="post" action="agenda.php">
-				<p>Nombre</p><input type="text" name="nombre">
-				<p>Correo</p><input type="email" name="correo">
+				<p>Nombre</p><input type="text" name="nombre"<?php if(isset($_POST["nombre"])){echo "value='".$_POST["nombre"]."'";}?>>
+				<p>Correo</p><input type="email" name="correo"<?php if(isset($_POST["correo"])){echo "value='".$_POST["correo"]."'";}?>>
 				<br><br>
 				<input type="submit" name="guardar" value="guardar datos">
 			</form>
@@ -38,8 +45,14 @@
 						if($_POST["guardar"]){
 							$nombre=strtolower($_POST["nombre"]);
 							$correo=strtolower($_POST["correo"]);
-							if($nombre=="" && $correo==""){
-								echo "inserte datos en los campos por favor.";
+							if($nombre=="" ){
+								echo " INSERTE EL NOMBRE POR FAVOR !!!";
+								echo "<hr>";
+								foreach ($agenda as $key => $value){
+										echo "Nombre: ".$key."<br>";
+										echo "Correo: ".$value."<br>";
+										echo "<hr>";
+								}
 							}elseif(array_key_exists($nombre,$agenda)){
 								if($correo==""){
 									unset($agenda[$nombre]);
@@ -72,9 +85,7 @@
 						}
 						$_SESSION["agenda"]=$agenda;
 					}
-
-					
-					?>
+				?>
 		</div>
 	</body>
 </html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>index</title>
+		<style type="text/css">
+			#formulario{
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="formulario">
+			<h2>Inserte tu nombre: </h2>
+			<form action="agenda.php" method="POST">
+				<input type="text" name="nombreIndex">
+				<input type="submit" name="envioIndex" value="Iniciar Sesion">
+			</form>
+		</div>
+	</body>
+</html>
